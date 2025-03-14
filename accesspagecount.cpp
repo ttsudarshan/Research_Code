8c8a84e38b31775e1dc626842ef4fa5845cbdb0e
Sudarshan <159210386+ttsudarshan@users.noreply.github.com>
2025-03-13 19:24:03 -0500

    new code

--- accesspagecount.cpp
+++ accesspagecount.cpp
@@ -1,93 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
 
-#define PAGE_SIZE 4096  // 4KB pages
-#define NUM_TRACKED_PAGES 10  // Track 10 randomly chosen pages
+#define MEMORY_SIZE_GB 7
+#define PAGE_SIZE 4096
 
-void displayPageAccessInfo(char *memory, size_t total_pages, size_t *tracked_pages, int *access_count) {
-    unsigned char *vec = (unsigned char *)calloc(total_pages, sizeof(unsigned char));
-    if (!vec) {
-        perror("calloc failed");
-        return;
-    }
+using namespace std;
 
-    while (1) {
-        if (mincore(memory, total_pages * PAGE_SIZE, vec) == 0) {
-            printf("\n=== Page Access Summary (Every 5 sec) ===\n");
-            for (size_t i = 0; i < NUM_TRACKED_PAGES; i++) {
-                size_t page_index = tracked_pages[i];
-                if (vec[page_index] & 1) {
-                    access_count[i]++;
-                    printf("Page %zu (Real Address: %p) Accessed %d times\n",
-                           page_index, (void *)&memory[page_index * PAGE_SIZE], access_count[i]);
-                }
-            }
-        } else {
-            perror("mincore failed");
-        }
-        sleep(5);
-    }
-    free(vec);
-}
+int main(int argc, char **argv)
+{
+    int i, j;
+    int success = 0;
+    int fail = 0;
+    int num_pages;
 
-int main(int argc, char **argv) {
+    // Convert the input argument to integer
     if (argc < 2) {
-        printf("Usage: %s <memory_size_in_GB>\n", argv[0]);
-        return 1;
+        printf("no memory defined");
+        return -1;
     }
 
-    int mem_size_gb = atoi(argv[1]);
-    if (mem_size_gb <= 0) {
-        printf("Invalid memory size.\n");
-        return 1;
-    }
+    num_pages = 256 * 1024 * atoi(argv[1]); // Total number of pages to access
 
-    printf("Allocating %d GB of memory...\n", mem_size_gb);
-    
-    size_t total_pages = (mem_size_gb * 256 * 1024);
-    char *memory = (char *)mmap(NULL, total_pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (memory == MAP_FAILED) {
-        perror("Memory allocation failed");
-        return 1;
-    }
+    // Print the requested memory size
+    printf("Accessing pages for: %d GB\n", atoi(argv[1]));
 
-    int success = 0, fail = 0;
-
-    // Access all allocated pages
-    for (size_t i = 0; i < total_pages; i++) {
-        char *ptr = &memory[i * PAGE_SIZE];
-        if (!ptr) {
+    // Allocate memory pages
+    char *ptr[num_pages];
+    for (i = 0; i < num_pages; i++) {
+        ptr[i] = (char *)malloc(PAGE_SIZE);
+        if (ptr[i] == NULL) {
+            printf("Memory allocation failed at page %d\n", i);
             fail++;
         } else {
-            memset(ptr, 'a', PAGE_SIZE);  // Access memory to bring it into RAM
+            // Initialize the page with a value to ensure it is not optimized away
+            for (j = 0; j < PAGE_SIZE; j++) {
+                ptr[i][j] = 'a';
+            }
             success++;
         }
     }
 
-    printf("Success: %d, Failed: %d\n", success, fail);
+    printf("Successfully allocated pages: %d, Allocation failures: %d\n", success, fail);
 
-    // Randomly select 10 pages to track
-    srand(time(NULL));
-    size_t tracked_pages[NUM_TRACKED_PAGES];
-    int access_count[NUM_TRACKED_PAGES] = {0};  // Initialize access counts to zero
-    for (size_t i = 0; i < NUM_TRACKED_PAGES; i++) {
-        tracked_pages[i] = rand() % total_pages;
+    // Repeatedly access the memory pages until the program is killed
+    while (1) {
+        for (i = 0; i < num_pages; i++) {
+            // Access each page (simple read operation)
+            char temp = ptr[i][0];
+        }
     }
 
-    printf("\nTracking %d Random Pages for Access Frequency:\n", NUM_TRACKED_PAGES);
-    for (size_t i = 0; i < NUM_TRACKED_PAGES; i++) {
-        printf("Tracking Page %zu (Address: %p)\n", tracked_pages[i], (void *)&memory[tracked_pages[i] * PAGE_SIZE]);
-    }
-
-    // Start tracking accessed pages
-    displayPageAccessInfo(memory, total_pages, tracked_pages, access_count);
-
-    // Cleanup (this point will never be reached in the infinite loop)
-    munmap(memory, total_pages * PAGE_SIZE);
     return 0;
 }
--- mem_pressure_access.c
+++ mem_pressure_access.c
@@ -1,74 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <errno.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define PAGE_SIZE 4096  // 4KB pages
+#define MEMORY_SIZE_GB 7
 
-void displayPageAccessInfo(char *memory, size_t total_pages) {
-    unsigned char *vec = (unsigned char *)calloc(total_pages, sizeof(unsigned char));
-    if (!vec) {
-        perror("calloc failed");
-        return;
-    }
+int main(int argc, char **argv)
+{
 
-    while (1) {
-        if (mincore(memory, total_pages * PAGE_SIZE, vec) == 0) {
-            printf("\nPage Access Summary:\n");
-            for (size_t i = 0; i < total_pages; i++) {
-                if (vec[i] & 1) {
-                    printf("Page %zu is in RAM\n", i);
-                }
-            }
-        } else {
-            perror("mincore failed");
-        }
-        sleep(5);
-    }
-    free(vec);
-}
+	int i, j;
+        int sccss=0;
+        int fail=0;
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("Usage: %s <memory_size_in_GB>\n", argv[0]);
-        return 1;
-    }
+        printf("received: %d GB\n", atoi(argv[1]));
 
-    int mem_size_gb = atoi(argv[1]);
-    if (mem_size_gb <= 0) {
-        printf("Invalid memory size.\n");
-        return 1;
-    }
+        for(;i<256*1024*atoi(argv[1]);i++)
+	{
+		char *ptr = (char *)malloc(4096);
+		if(!ptr) 
+			fail++;
+		else
+		{
+			for(j=0;j<4090;j++)
+				ptr[j]='a';
+			sccss++;
+		}
 
-    printf("Allocating %d GB of memory...\n", mem_size_gb);
-    
-    size_t total_pages = (mem_size_gb * 256 * 1024);
-    char *memory = (char *)mmap(NULL, total_pages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (memory == MAP_FAILED) {
-        perror("Memory allocation failed");
-        return 1;
-    }
+	}
 
-    int success = 0, fail = 0;
+	printf("susscess: %d, failed: %d\n", sccss, fail);
+	getchar();
 
-    for (size_t i = 0; i < total_pages; i++) {
-        char *ptr = &memory[i * PAGE_SIZE];
-        if (!ptr) {
-            fail++;
-        } else {
-            memset(ptr, 'a', PAGE_SIZE);  // Access memory to bring it into RAM
-            success++;
-        }
-    }
 
-    printf("Success: %d, Failed: %d\n", success, fail);
-
-    // Start tracking accessed pages
-    displayPageAccessInfo(memory, total_pages);
-
-    // Cleanup (this point will never be reached in the infinite loop)
-    munmap(memory, total_pages * PAGE_SIZE);
-    return 0;
 }
